b691f716fbeeef9475a72d36e1c3d151c10941d7
Adrià Gual <agualandreu@gmail.com>
2022-07-01 19:31:32 +0200

    v1.06 - Gitfts

--- src/components/form/BaseSelect.vue
+++ src/components/form/BaseSelect.vue
@@ -26,7 +26,7 @@ function getUID() {
         <select :id="uuid" :value="modelValue" class="input-field" v-bind="$attrs"
             @change="updateValue($event.target.value)">
             <option v-for="option in options" :value="option.name" :key="option" :selected="option === modelValue">
-                <span>{{ option.name }}</span>
+                <span>{{ option }}</span>
             </option>
         </select>
     </div>
@@ -36,5 +36,16 @@ function getUID() {
 label {
     display: block;
     margin: 0px 0px 3px 4px;
+}
+
+select {
+    text-transform: capitalize;
+    font-size: 1.1rem;
+    letter-spacing: .2px;
+    cursor: pointer;
+}
+
+span {
+    cursor: pointer;
 }
 </style>
--- src/views/Gifts.vue
+++ src/views/Gifts.vue
@@ -15,63 +15,130 @@ onMounted(() => {
 <template>
     <h1 class="main-title">Regalos amigo invisible</h1>
     <p class="span-color">Página en desarollo</p>
-    <h2>¿Necesitas ideas para el <span class="span-color">regalo del amigo invisible</span>? ¡No pierdas más el tiempo!</h2>
+    <h2>¿Necesitas ideas para el <span class="span-color">regalo del amigo invisible</span>? ¡No pierdas más el tiempo!
+    </h2>
     <p>Aquí tienes algunos productos para que puedas comprarlos directamente en Amazon con todos sus beneficios y al
         mejor precio.</p>
-    <input class="input-field" type="search" placeholder="Buscar productos, categorías..." />
+
+    <!-- SEARCH -->
+    <!-- <input class="input-field" type="search" placeholder="Buscar productos, categorías..." /> -->
 
     <!-- CATEGORIES ASIDE -->
-    <aside class="categories">
-        <CheckBox label="Todos" />
-        <CheckBox label="Videojuegos" />
-        <CheckBox label="Regalos" />
-        <CheckBox label="Electrónica" />
-        <CheckBox label="Tarjetas regalo" />
-        <CheckBox label="Ropa" />
-        <CheckBox label="Accesorios" />
-        <CheckBox label="Otros" />
-    </aside>
+    <section class="categories">
+        <p>Selecciona la categoría</p>
+        <BaseSelect :options="getCategoryNames" v-model="categorySelected" />
+    </section>
 
-    <!-- CATEGORIES AND PRODUCTS CONTAINERS -->
     <!-- PRODUCTS  -->
+    <!-- TODO: CREATE COMPONENT TO SHOW ALL THE COMPONETS SENDING PROPS LIKE PRICE, DESC, LINK... -->
+    <!-- TODO: INFINITE SCROLL -->
     <section class="products">
-        <!-- TODO: CREATE COMPONENT TO SHOW ALL THE COMPONETS SENDING PROPS LIKE PRICE, DESC, LINK... -->
-        <!-- TODO: INFINITE SCROLL -->
-        <iframe sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin"
-            style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0"
-            src="//rcm-eu.amazon-adsystem.com/e/cm?lt1=_blank&bc1=FFFFFF&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=arkox-21&language=es_ES&o=30&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B098RJXBTY&linkId=3f8f303d2d195507dde6b328eff9a1df"></iframe>
-
-        <iframe sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin"
-            style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0"
-            src="//rcm-eu.amazon-adsystem.com/e/cm?lt1=_blank&bc1=FFFFFF&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=arkox-21&language=es_ES&o=30&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B09GVZBYM2&linkId=16b2c31b29e28e65977563314249857b"></iframe>
+        <template v-for="(item, index) in getProductsCategorySelected" :key="index">
+            <div class="product-item" :class="'product- ' + index" v-html="item"></div>
+        </template>
     </section>
 </template>
 
 <script>
-import CheckBox from '../components/form/BaseCheckBox.vue';
+import BaseSelect from '../components/form/BaseSelect.vue';
 
 export default {
-    components: { CheckBox },
+    components: { BaseSelect },
     data() {
         return {
+            categorySelected: 'todos',
             categories: [
-                { name: 'Videojuegos', id: 'videojuegos', products: [] },
-                { name: 'Deportes', id: 'deportes', products: [] },
-                { name: 'Ropa', id: 'ropa', products: [] },
-                { name: 'Joyería', id: 'joyeria', products: [] },
-                { name: 'Hogar y cocina', id: 'hogar-y-cocina', products: [] },
-                { name: 'Bricolaje', id: 'bricolaje', products: [] },
-                { name: 'Cheques regalo', id: 'cheques-regalo', products: [] }
+                {
+                    name: 'todos',
+                    id: 'todos',
+                    products: [
+                        '<iframe sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin"style="width:120px;height:240px;"marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//rcm-eu.amazon-adsystem.com/e/cm?lt1=_blank&bc1=FFFFFF&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=arkox-21&language=es_ES&o=30&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B098RJXBTY&linkId=3f8f303d2d195507dde6b328eff9a1df"></iframe>',
+
+                        '<iframe sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin" style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//rcm-eu.amazon-adsystem.com/e/cm?lt1=_blank&bc1=FFFFFF&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=arkox-21&language=es_ES&o=30&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B09GVZBYM2&linkId=16b2c31b29e28e65977563314249857b"></iframe>',
+
+                        '<iframe sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin" style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//rcm-eu.amazon-adsystem.com/e/cm?lt1=_blank&bc1=FFFFFF&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=arkox-21&language=es_ES&o=30&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B08ZL43KS4&linkId=d69ad083475475e888c6f4bcca95e795"></iframe>',
+
+                        '<iframe sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin" style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//rcm-eu.amazon-adsystem.com/e/cm?lt1=_blank&bc1=FFFFFF&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=arkox-21&language=es_ES&o=30&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B0832WLDLC&linkId=a9553e9beaa902c5beba54d1b3597240"></iframe>',
+
+                        '<iframe sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin" style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//rcm-eu.amazon-adsystem.com/e/cm?lt1=_blank&bc1=FFFFFF&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=arkox-21&language=es_ES&o=30&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B007WPOA3Y&linkId=5da0fb137071a7424a3bf604e708d47b"></iframe>',
+
+                        '<iframe sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin" style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//rcm-eu.amazon-adsystem.com/e/cm?lt1=_blank&bc1=FFFFFF&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=arkox-21&language=es_ES&o=30&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B07X2QFSKR&linkId=22ba2c88123d6c79ba88cda258ae2265"></iframe>',
+
+                        '<iframe sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin" style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//rcm-eu.amazon-adsystem.com/e/cm?lt1=_blank&bc1=FFFFFF&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=arkox-21&language=es_ES&o=30&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B07S6NTCLB&linkId=e2444d8ffb00df7d0994865e23d939ef"></iframe>',
+
+                        '<iframe sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin" style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//rcm-eu.amazon-adsystem.com/e/cm?lt1=_blank&bc1=FFFFFF&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=arkox-21&language=es_ES&o=30&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B09N355H4Z&linkId=0869f981bed76fc055191e18e432a8fd"></iframe>',
+
+                        '<iframe sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin" style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//rcm-eu.amazon-adsystem.com/e/cm?lt1=_blank&bc1=FFFFFF&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=arkox-21&language=es_ES&o=30&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B0B3XMH5SB&linkId=af948426043eec4bcff738ab16df982e"></iframe>',
+
+                        '<iframe sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin" style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//rcm-eu.amazon-adsystem.com/e/cm?lt1=_blank&bc1=FFFFFF&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=arkox-21&language=es_ES&o=30&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B08HP4513K&linkId=ca2855f5b4af9043471d2a96e4b6924c"></iframe>',
+
+                        '<iframe sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin" style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//rcm-eu.amazon-adsystem.com/e/cm?lt1=_blank&bc1=FFFFFF&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=arkox-21&language=es_ES&o=30&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B07JH6VD7T&linkId=2da5844dd0686d50a77e76df10bbaf58"></iframe>',
+
+                        '<iframe sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin" style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//rcm-eu.amazon-adsystem.com/e/cm?lt1=_blank&bc1=FFFFFF&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=arkox-21&language=es_ES&o=30&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B09S5S9L94&linkId=1f7ce916b2a0fb7360d97fe65c8ad09e"></iframe>',
+
+                        '<iframe sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin" style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//rcm-eu.amazon-adsystem.com/e/cm?lt1=_blank&bc1=FFFFFF&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=arkox-21&language=es_ES&o=30&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B08881PMQB&linkId=1cb1a0bc37c4fabbf32fce16e9f6fa43"></iframe>',
+
+                        '<iframe sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin" style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//rcm-eu.amazon-adsystem.com/e/cm?lt1=_blank&bc1=FFFFFF&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=arkox-21&language=es_ES&o=30&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B0041R9NMO&linkId=97b2f764d5c616d73a6f28dec5241f5e"></iframe>',
+
+                        '<iframe sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin" style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//rcm-eu.amazon-adsystem.com/e/cm?lt1=_blank&bc1=FFFFFF&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=arkox-21&language=es_ES&o=30&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B06Y49XN6Q&linkId=6037fb94fa78e506ff8722e5ef0a12e1"></iframe>',
+
+                        '<iframe sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin" style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//rcm-eu.amazon-adsystem.com/e/cm?lt1=_blank&bc1=FFFFFF&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=arkox-21&language=es_ES&o=30&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B09LCVHCVX&linkId=aa7065c7efa3a376b77d8d8e433fcc4b"></iframe>',
+
+
+                        '<iframe sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin" style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//rcm-eu.amazon-adsystem.com/e/cm?lt1=_blank&bc1=FFFFFF&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=arkox-21&language=es_ES&o=30&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B073V1SPHP&linkId=d550f574d3c7d548412bfac9ea994c5d"></iframe>',
+
+                        '<iframe sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin" style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//rcm-eu.amazon-adsystem.com/e/cm?lt1=_blank&bc1=FFFFFF&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=arkox-21&language=es_ES&o=30&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B08Q7D492K&linkId=f0a69847369715214ed015e04d6d7a5a"></iframe>',
+
+                        '<iframe sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin" style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//rcm-eu.amazon-adsystem.com/e/cm?lt1=_blank&bc1=FFFFFF&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=arkox-21&language=es_ES&o=30&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B077GL32M6&linkId=8d0d0881feb28c9ff582c25641f078dc"></iframe>',
+
+                        '<iframe sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin" style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//rcm-eu.amazon-adsystem.com/e/cm?lt1=_blank&bc1=FFFFFF&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=arkox-21&language=es_ES&o=30&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B09DSNXV9K&linkId=dd3aef591a88521c833134221b89cc0b"></iframe>',
+
+                        '<iframe sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin" style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//rcm-eu.amazon-adsystem.com/e/cm?lt1=_blank&bc1=FFFFFF&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=arkox-21&language=es_ES&o=30&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B07PZ6B5YP&linkId=eae0233b4e6dca0db89c9aa43f3734b4"></iframe>',
+
+                        '<iframe sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin" style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//rcm-eu.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=arkox-21&language=es_ES&o=30&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B07CXKK75M&linkId=51aa9997a8e9f6e68821b29c97c2c635"></iframe>',
+
+                        '<iframe sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin" style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//rcm-eu.amazon-adsystem.com/e/cm?lt1=_blank&bc1=FFFFFF&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=arkox-21&language=es_ES&o=30&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B07R3DBQ6Z&linkId=69f4361e0037d2230b735347d25d250d"></iframe>',
+
+                        '<iframe sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin" style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//rcm-eu.amazon-adsystem.com/e/cm?lt1=_blank&bc1=FFFFFF&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=arkox-21&language=es_ES&o=30&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B07YNZ8XZ2&linkId=8b64c49ea9e851dfec644b90ae703fe4"></iframe>',
+
+                        '<iframe sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin" style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//rcm-eu.amazon-adsystem.com/e/cm?lt1=_blank&bc1=FFFFFF&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=arkox-21&language=es_ES&o=30&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B07DXXS2ZJ&linkId=201f79a2cc8a95c5fa94b2e2509444f5"></iframe>',
+                    ]
+                },
+                { name: 'videojuegos', id: 'videojuegos', products: [] },
+                { name: 'deportes', id: 'deportes', products: [] },
+                { name: 'ropa', id: 'ropa', products: [] },
+                { name: 'joyería', id: 'joyeria', products: [] },
+                { name: 'hogar y cocina', id: 'hogar-y-cocina', products: [] },
+                { name: 'bricolaje', id: 'bricolaje', products: [] },
+                {
+                    name: 'cheques regalo', id: 'cheques-regalo', products: [
+                        '<iframe sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin" style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//rcm-eu.amazon-adsystem.com/e/cm?lt1=_blank&bc1=000000&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=arkox-21&language=es_ES&o=30&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B07CXKK75M&linkId=51aa9997a8e9f6e68821b29c97c2c635"></iframe>',
+
+                        '<iframe sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin" style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//rcm-eu.amazon-adsystem.com/e/cm?lt1=_blank&bc1=FFFFFF&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=arkox-21&language=es_ES&o=30&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B07R3DBQ6Z&linkId=69f4361e0037d2230b735347d25d250d"></iframe>',
+
+                        '<iframe sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin" style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//rcm-eu.amazon-adsystem.com/e/cm?lt1=_blank&bc1=FFFFFF&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=arkox-21&language=es_ES&o=30&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B07YNZ8XZ2&linkId=8b64c49ea9e851dfec644b90ae703fe4"></iframe>',
+
+                        '<iframe sandbox="allow-popups allow-scripts allow-modals allow-forms allow-same-origin" style="width:120px;height:240px;" marginwidth="0" marginheight="0" scrolling="no" frameborder="0" src="//rcm-eu.amazon-adsystem.com/e/cm?lt1=_blank&bc1=FFFFFF&IS2=1&bg1=FFFFFF&fc1=000000&lc1=0000FF&t=arkox-21&language=es_ES&o=30&p=8&l=as4&m=amazon&f=ifr&ref=as_ss_li_til&asins=B07DXXS2ZJ&linkId=201f79a2cc8a95c5fa94b2e2509444f5"></iframe>',
+                    ]
+                }
             ]
         }
     },
+    computed: {
+        getProductsCategorySelected() {
+            return this.categories.find(category => category.name == this.categorySelected).products;
+        },
+        getCategoryNames() {
+            return this.categories.map(category => category.name);
+        }
+    }
 }
 
 </script>
 
 <style scoped>
-
-h2, p {
+h2,
+p {
     margin-bottom: 2rem;
 }
 
@@ -91,23 +158,38 @@ input[type=search]:focus {
 /* END SEARCH INPUT */
 
 /* CATEGORIES ASIDE */
-.categories {
-    flex-basis: 28%;
-    border: 1px solid yellow;
-    border-radius: var(--border-radius);
-    padding: 1.1rem;
-    position: absolute;
-    left: -30%;
-    top: 32.5%;
+.categories p {
+    margin-bottom: 0;
+    padding: 0px 10px;
+}
+
+.categories>div {
+    padding: 10px;
 }
 
 /* PRODUCTS */
 .products {
-    flex-basis: 68%;
-    border: 1px solid pink;
-    height: 10rem;
-    display: block;
-    padding: 5px;
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: center;
     margin: 2rem 0;
+}
+
+.product-item {
+    padding: 1rem;
+}
+
+/* AMAZON */
+
+@media only screen and (max-width: 768px) {
+    .product-item {
+        padding: .2rem
+    }
+}
+
+@media only screen and (max-width: 450px) {
+    .products {
+        justify-content: space-evenly;
+    }
 }
 </style>
