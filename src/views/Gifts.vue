2fef2035fb7150b7c12b4bf7fa75532beeeed1fa
Adrià Gual <agualandreu@gmail.com>
2022-07-05 09:45:02 +0200

    v1.06 - Gitfts

--- src/views/Gifts.vue
+++ src/views/Gifts.vue
@@ -25,7 +25,7 @@ onMounted(() => {
 
     <!-- CATEGORIES ASIDE -->
     <section class="categories">
-        <BaseSelect label="Selecciona la categoría" :options="getCategoryNames" v-model="this.categorySelected" />
+        <BaseSelect label="Selecciona la categoría" :options="getCategoryNames" v-model="categorySelected" />
     </section>
 
     <!-- PRODUCTS  -->
